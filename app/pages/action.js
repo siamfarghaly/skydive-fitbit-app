8ffef44e301b6719c7c0ef8944393d79e52c5eb3
Siam Farghaly <siam@MacBook-Pro-van-Siam.local>
2020-11-06 23:32:45 +0100

    cleanup init function

--- app/pages/action.js
+++ app/pages/action.js
@@ -7,12 +7,11 @@ import { Barometer } from "barometer";
 
 var altitudeLabel;
 var distanceLabel;
+var lzLong;
+var lzLat;
+var currentLong;
+var currentLat;
 
-
-//*******************************
-//for onclick 'START' button, saves coordinates of LZ to variable
-var lzLong = 0;
-var lzLat = 0;
 
 export function destroy() {
   console.log('destroy action page');
@@ -20,13 +19,22 @@ export function destroy() {
   distanceLabel = null;
   lzLong = null;
   lzLat = null;
+  currentLong = null;
+  currentLat = null;
 }
 
 export function init() {
   console.log('init action page');
   altitudeLabel = document.getElementById('altitude');
   distanceLabel = document.getElementById('distanceLZ');
+  watchDistanceLZ();
+  startAltimeter()
+};
 
+
+function watchDistanceLZ(){
+
+  //get current location, create Landing Zone coordinates
   geolocation.getCurrentPosition(locationSuccess, locationError, {
     timeout: 60 * 1000
   });
@@ -43,14 +51,11 @@ export function init() {
                 "Message: " + error.message);
   }
 
-  //**********************************
-
-
-  var currentLong;
-  var currentLat;
 
   //watch current GPS coordinates + calculate distance LZ
-  var watchID = geolocation.watchPosition(watchLocation, watchLocationError, { timeout: 60 * 1000 });
+  var watchID = geolocation.watchPosition(watchLocation, locationError, {
+    timeout: 60 * 1000
+  });
 
   function watchLocation(position) {
       currentLat = position.coords.latitude;
@@ -59,12 +64,10 @@ export function init() {
                   "Current Longitude: " + position.coords.longitude);
       distanceLabel.text = distance(currentLat,currentLong,lzLat,lzLong);
   }
+}
 
-  function watchLocationError(error) {
-    console.log("Error: " + error.code,
-                "Message: " + error.message);
-  }
 
+function startAltimeter(){
   // Create Barometer, 1 reading per second
   var bar = new Barometer({ frequency: 1 });
 
@@ -73,38 +76,36 @@ export function init() {
     altitudeLabel.text = altitudeFromPressure(bar.pressure / 100) + " ft";
   }
 
-  // Begin monitoring the sensor
   bar.start();
+}
 
 
-  // Converts pressure in millibars to altitude in feet
-  // https://en.wikipedia.org/wiki/Pressure_altitude
-  function altitudeFromPressure(pressure) {
-    return (1 - (pressure/1013.25)**0.190284)*145366.45;
+// Converts pressure in millibars to altitude in feet
+// https://en.wikipedia.org/wiki/Pressure_altitude
+function altitudeFromPressure(pressure) {
+  return (1 - (pressure/1013.25)**0.190284)*145366.45;
+}
+
+
+//calculates distance between 2 coordinates
+//https://www.geodatasource.com/developers/javascript
+function distance(lat1, lon1, lat2, lon2) {
+  if ((lat1 == lat2) && (lon1 == lon2)) {
+    return 0;
   }
-
-
-  //calculates distance between 2 coordinates
-  //https://www.geodatasource.com/developers/javascript
-  function distance(lat1, lon1, lat2, lon2) {
-  	if ((lat1 == lat2) && (lon1 == lon2)) {
-  		return 0;
-  	}
-    else {
-  		var radlat1 = Math.PI * lat1/180;
-  		var radlat2 = Math.PI * lat2/180;
-  		var theta = lon1-lon2;
-  		var radtheta = Math.PI * theta/180;
-  		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-  		if (dist > 1) {
-  			dist = 1;
-  		}
-  		dist = Math.acos(dist);
-  		dist = dist * 180/Math.PI;
-  		dist = dist * 60 * 1.1515;
-  		dist = dist * 1.609344
-  		return dist;
-  	}
+  else {
+    var radlat1 = Math.PI * lat1/180;
+    var radlat2 = Math.PI * lat2/180;
+    var theta = lon1-lon2;
+    var radtheta = Math.PI * theta/180;
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = dist * 180/Math.PI;
+    dist = dist * 60 * 1.1515;
+    dist = dist * 1.609344
+    return dist;
   }
-
-};
+}
--- app/pages/index.js
+++ app/pages/index.js
@@ -3,10 +3,6 @@ import { switchPage } from '../navigation';
 
 let $buttonAction = null;
 
-function doSomething() {
-  console.log('hallo index');
-}
-
 export function destroy() {
   console.log('destroy index page');
   $buttonAction = null;
@@ -15,10 +11,7 @@ export function destroy() {
 export function init() {
   console.log('init index page');
   $buttonAction = document.getElementById('action-button');
-
   $buttonAction.onclick = () => {
     switchPage('action', true);
   };
-
-  doSomething();
 }
